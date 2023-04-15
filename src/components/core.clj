470374a963de760337d2162918be32af866939a0
CarnunMP <carnun@hotmail.co.uk>
2023-04-16 00:09:09 +0100

    Clean up; Move into /src dir; Add header 'component'

---
+++ src/components/core.clj
@@ -0,0 +1,10 @@
+(ns components.core)
+
+(def header
+  [:div.header
+   [:div.logo
+    "WattDo?X"]
+   [:div.nav
+    [:a {:href "./pages/pricing.html"} "Pricing"]
+    [:a {:href "./pages/pricing.html"} "Foo"]
+    [:a {:href "./pages/pricing.html"} "Bar"]]])
