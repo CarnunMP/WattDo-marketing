7add673e68eb6002dabe96127647c30e1ea646d5
CarnunMP <admin@carnun.com>
2023-04-27 15:04:09 +0100

    Add platform buttons + CTA

--- src/pages/index.clj
+++ src/pages/index.clj
@@ -8,4 +8,17 @@
    [:div.banner
     [:h1 "WattDo?⚡"]
     (core/card {:text "capture and direct moments of inspiration towards ambitious, emergent ends"
-                :tags [:fun :flow :productivity]})]])
+                :tags [:fun :flow :productivity]})]
+   [:div.ctas
+    [:div.platforms
+     [:div.platform-button
+      [:img {:src "../assets/web.png"}]
+      [:p "COMING SOON"]]
+     [:div.platform-button
+      [:img {:src "../assets/apple.svg"}]
+      [:p "COMING SOON"]]
+     [:div.platform-button
+      [:img {:src "../assets/android.png"}]
+      [:p "COMING SOON"]]]
+    [:a.waitlist {:href "https://iphg8uqckwl.typeform.com/to/HeB32ndu"}
+     [:h3 "join the waitlist"]]]])
