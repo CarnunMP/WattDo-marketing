4846c620de9d627b39d292e304396f6e9e7854cf
CarnunMP <carnun@hotmail.co.uk>
2023-07-04 22:52:33 +0100

    WattDo?⚡ -> watt.do⚡

--- bin/gen.clj
+++ bin/gen.clj
@@ -13,7 +13,7 @@
        [:head
         [:meta {:charset "utf-8"}]
         [:meta {:name "viewport" :content "width=device-width,initial-scale=1"}]
-        [:title "WattDo?⚡"]
+        [:title "watt.do⚡"]
         [:link {:rel "preconnect", :href "https://fonts.googleapis.com"}]
         [:link {:rel "preconnect", :href="https://fonts.gstatic.com" :crossorigin true}]
         [:link {:rel "stylesheet" :href "https://fonts.googleapis.com/css2?family=Electrolize&display=swap"}]
